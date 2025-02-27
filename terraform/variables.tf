5c54ed8773b9ce5d831dfbdb37c6341e77c10a21
ithotgi <itthisgo@gmail.com>
2025-02-27 17:36:53 +0900

    terraform, ansible 5

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "ec2_ssh_key" {
+  description = "EC2 SSH Public Key"
+  type        = string
+  sensitive   = true
+}
+
 variable "key_name" {
   description = "AWS EC2 SSH Key Pair"
   type        = string
