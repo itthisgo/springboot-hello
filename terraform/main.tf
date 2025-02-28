d91aaee26329ff1d6cbb857c54aa08bea7fa91df
ithotgi <itthisgo@gmail.com>
2025-02-28 09:48:25 +0900

    terraform, ansible 8

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "ap-northeast-2"  # 서울 리전
 }
 
+# 기본 VPC 가져오기
+data "aws_vpc" "default" {
+  id = "vpc-0a26da92d28ec58b9"
+}
+
+# 기본 VPC 내에서 사용 가능한 서브넷 가져오기
+data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 # 기존 보안 그룹이 있는지 확인
 data "aws_security_group" "existing_sg" {
   filter {
@@ -10,11 +23,12 @@ data "aws_security_group" "existing_sg" {
   }
 }
 
-# 없으면 새로 생성
+# 보안 그룹이 없을 경우 새로 생성
 resource "aws_security_group" "my_sg" {
   count       = length(data.aws_security_group.existing_sg.id) > 0 ? 0 : 1
   name        = "springboot-hello-sg"
   description = "Allow inbound traffic"
+  vpc_id      = data.aws_vpc.default.id  # 기본 VPC에 배치
 
   ingress {
     from_port   = 22
@@ -36,6 +50,13 @@ resource "aws_security_group" "my_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # EC2 인스턴스 생성 (보안 그룹 동적 적용)
@@ -43,7 +64,8 @@ resource "aws_instance" "my_server" {
   ami                    = "ami-0077297a838d6761d"  # 최신 Ubuntu AMI
   instance_type          = "t2.micro"
   key_name               = var.key_name
-  security_group_ids     = length(data.aws_security_group.existing_sg.id) > 0 ? [data.aws_security_group.existing_sg.id] : [aws_security_group.my_sg[0].id]
+  subnet_id              = data.aws_subnet.default.id  # 기본 VPC의 서브넷 사용
+  vpc_security_group_ids = length(data.aws_security_group.existing_sg.id) > 0 ? [data.aws_security_group.existing_sg.id] : [aws_security_group.my_sg[0].id]
   associate_public_ip_address = true  # 퍼블릭 IP 할당
 
   tags = {
