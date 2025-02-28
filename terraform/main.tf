8a3181205c59bdac25f4a35d4492db676ffe8238
ithotgi <itthisgo@gmail.com>
2025-02-28 09:45:46 +0900

    terraform, ansible 8

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "ap-northeast-2"  # 서울 리전
 }
 
+# 기존 보안 그룹이 있는지 확인
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["springboot-hello-sg"]
+  }
+}
+
+# 없으면 새로 생성
 resource "aws_security_group" "my_sg" {
+  count       = length(data.aws_security_group.existing_sg.id) > 0 ? 0 : 1
   name        = "springboot-hello-sg"
   description = "Allow inbound traffic"
 
@@ -28,11 +38,12 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
+# EC2 인스턴스 생성 (보안 그룹 동적 적용)
 resource "aws_instance" "my_server" {
-  ami                  = "ami-0077297a838d6761d"  # 최신 Ubuntu AMI 사용 (변경 필요)
-  instance_type        = "t2.micro"
-  key_name            = var.key_name
-  security_groups      = [aws_security_group.my_sg.name]
+  ami                    = "ami-0077297a838d6761d"  # 최신 Ubuntu AMI
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  security_group_ids     = length(data.aws_security_group.existing_sg.id) > 0 ? [data.aws_security_group.existing_sg.id] : [aws_security_group.my_sg[0].id]
   associate_public_ip_address = true  # 퍼블릭 IP 할당
 
   tags = {
@@ -40,6 +51,7 @@ resource "aws_instance" "my_server" {
   }
 }
 
+# 생성된 EC2의 Public IP 출력
 output "ec2_public_ip" {
   value = aws_instance.my_server.public_ip
 }
